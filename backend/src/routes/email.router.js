a088cfff7e490debc6fc8d16f23f87fda7d1417f
joaquinnn26 <joaquinfefe@gmail.com>
2024-04-15 13:10:48 -0300

    separando back

--- backend/src/routes/email.router.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { emailController } from "../controllers/email.controllers.js";
-const router=Router()
-
-router.post("/send",emailController)
-
-export default router
